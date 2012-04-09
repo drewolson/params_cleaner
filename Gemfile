2031ed2ccc9b2b53dfdaf88649d2a2446e0a19da
Drew Olson <drew@drewolson.org>
2012-04-09 08:28:12 -0500

    Initial commit.
    Extracted from https://github.com/drewolson/elovation.git

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source "http://rubygems.org"
+
+gem "active_support", "~>3.0.0"
+
+group :development do
+  gem "rspec", "2.9.0"
+end
---
+++ lib/params_cleaner.rb
@@ -0,0 +1,20 @@
+require "active_support"
+require "active_support/core_ext/hash/slice"
+
+module ParamsCleaner
+  extend ActiveSupport::Concern
+
+  def clean_params(root)
+    params[root].slice(*self.class._allowed_params[root])
+  end
+
+  module ClassMethods
+    def allowed_params(params_hash)
+      @allowed_params = params_hash
+    end
+
+    def _allowed_params
+      @allowed_params
+    end
+  end
+end
