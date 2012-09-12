32ad6f15da383f930c4927f14f6cdf982f7bef18
adam <adam.forsyth@getbraintree.com>
2012-09-11 21:01:24 -0500

    Rewrite whitelist.rb to simplify logic

--- lib/params_cleaner/whitelist.rb
+++ lib/params_cleaner/whitelist.rb
@@ -1,62 +1,24 @@
 module ParamsCleaner
   class Whitelist
     def initialize(whitelist)
-      @whitelist = whitelist
+      @nested_hash = whitelist[-1].kind_of?(Hash) ? whitelist[-1] : {}
+      @top_level_keys = @nested_hash.empty? ? whitelist : whitelist[0..-2]
+      @nested_keys = @nested_hash.keys
     end
 
-    def sanitize(params, top_level = true)
-      cleaned_params = params.map do |key, value|
-        if value.kind_of?(Hash)
-          _clean_hash(key, value)
-        elsif value.kind_of?(Array)
-          _clean_array(key, value)
-        else
-          _clean_value(key, value, top_level)
+    def sanitize(obj, parent = nil)
+      if obj.kind_of?(Hash)
+        whitelist = @nested_keys.dup
+        whitelist.concat(@top_level_keys) unless parent
+        whitelist.concat(@nested_hash[parent]) if @nested_keys.include?(parent)
+        cleaned = obj.map do |key, value|
+          [key, sanitize(value, key.to_sym)] if whitelist.include?(key.to_sym)
         end
-      end
-
-      cleaned_params_hash = Hash[cleaned_params]
-      HashWithIndifferentAccess.new(cleaned_params_hash)
-    end
-
-    def _allowed_nested
-      @whitelist.detect { |params_group| params_group.is_a?(Hash) } || {}
-    end
-
-    def _allowed_top_level
-      params_groups = []
-      @whitelist.each do |params_group|
-        unless params_group.is_a?(Hash)
-          params_groups << params_group
-        end
-      end
-      params_groups
-    end
-
-    def _clean_array(key, value)
-      cleaned_values = value.map do |sub_value|
-        if sub_value.is_a?(Hash)
-          _clean_hash(key, sub_value).last
-        else
-          sub_value
-        end
-      end
-      [key, cleaned_values]
-    end
-
-    def _clean_hash(key, value)
-      allowed_keys = value.slice(*_allowed_nested[key.to_sym])
-      clean_values = sanitize(allowed_keys, false)
-      [key, clean_values]
-    end
-
-    def _clean_value(key, value, top_level)
-      return [key, value] unless top_level
-
-      if _allowed_top_level.include?(key.to_sym)
-        [key, value]
+        HashWithIndifferentAccess.new(Hash[cleaned.compact])
+      elsif obj.kind_of?(Array)
+        obj.map { |item| sanitize(item, parent) }
       else
-        []
+        obj
       end
     end
   end
