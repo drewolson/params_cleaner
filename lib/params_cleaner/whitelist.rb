fd496a4b9d3bd9e20ad3c2d9e8b08a4251dec124
drew <drew@drewolson.org>
2012-09-16 09:41:51 -0500

    Small refactoring to new whitelist implementation.

--- lib/params_cleaner/whitelist.rb
+++ lib/params_cleaner/whitelist.rb
@@ -1,24 +1,40 @@
 module ParamsCleaner
   class Whitelist
     def initialize(whitelist)
-      @whitelist_hash = whitelist.last.kind_of?(Hash) ? whitelist.pop : {}
-      @top_level_keys = whitelist
+      @whitelist = whitelist
     end
 
-    def sanitize(obj, parent = nil)
-      if obj.kind_of?(Hash)
-        whitelist = @whitelist_hash.keys
-        whitelist.concat(@top_level_keys) unless parent
-        whitelist.concat(@whitelist_hash[parent]) if @whitelist_hash[parent]
-        cleaned = obj.map do |key, value|
-          [key, sanitize(value, key.to_sym)] if whitelist.include?(key.to_sym)
-        end
-        HashWithIndifferentAccess.new(Hash[cleaned.compact])
-      elsif obj.kind_of?(Array)
-        obj.map { |item| sanitize(item, parent) }
+    def sanitize(item, parent = nil)
+      if item.kind_of?(Hash)
+        _sanitize_hash(item, parent)
+      elsif item.kind_of?(Array)
+        item.map { |item| sanitize(item, parent) }
       else
-        obj
+        item
       end
+    end
+
+    def _sanitize_hash(hash, parent)
+      valid_keys = _valid_keys_for_parent(parent)
+      valid_pairs = hash.select { |key, value| valid_keys.include?(key.to_sym) }
+      sanitized_pairs = valid_pairs.map { |key, value| [key, sanitize(value, key.to_sym)] }
+
+      HashWithIndifferentAccess.new(Hash[sanitized_pairs])
+    end
+
+    def _top_level_keys
+      @top_level_keys ||= @whitelist.reject { |item| item.kind_of?(Hash) }
+    end
+
+    def _valid_keys_for_parent(parent)
+      _whitelist_hash.keys.tap do |keys|
+        keys.concat(_top_level_keys) if parent.nil?
+        keys.concat(_whitelist_hash[parent]) if _whitelist_hash.has_key?(parent)
+      end
+    end
+
+    def _whitelist_hash
+      @whitelist_hash ||= @whitelist.last.is_a?(Hash) ? @whitelist.last : {}
     end
   end
 end
